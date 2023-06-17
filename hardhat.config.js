272bb9eb9ec80e29437c572eecba9a3e848064ca
HeroBlackInk <drv1@outlook.com>
2023-06-17 14:20:37 +0800

    initialize

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  defaultNetwork: 'localhost'
+  defaultNetwork: 'blockscout',
+  networks: {
+    blockscout: {
+      url: process.env.BLOCKSCOUT_NETWORK,
+      accounts: [process.env.PrivateKey0]
+    },
+  }
 };
